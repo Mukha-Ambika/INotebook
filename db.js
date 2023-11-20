c9bbe0d6dc3ee2fed380d8bb4add3c99c0f62a3f
Mukha-Ambika <68109461+Mukha-Ambika@users.noreply.github.com>
2023-11-20 17:35:58 +0000

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURL = "mongodb://0.0.0.0:27017/inotebook"
+mongoose.set("strictQuery", false);
+
+async function connecToMongo() {
+    await mongoose.connect(mongoURL);
+    console.log("successfully connected to mongodb");
+  }
+
+module.exports = connecToMongo;
